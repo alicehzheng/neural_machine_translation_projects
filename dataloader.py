d8568b68ed176fca36db99d88bfecda5a24a95e3
Alice <alicehzheng@gmail.com>
2019-09-28 22:51:31 -0400

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -5,30 +5,30 @@ import numpy as np
 
 
 def collate(seqs):
-    # print(seqs)
+    #print(seqs)
     batch_size = len(seqs)
-    # print(batch_size)
+    #print(batch_size)
     seq_lengths = torch.zeros(batch_size, dtype=torch.int)
     max_seq_len = 0
     for i in range(0, batch_size):
         seq_lengths[i] = seqs[i][0].shape[0]
         max_seq_len = max(max_seq_len, seqs[i][0].shape[0])
-    # print(seq_lengths)
+    
+    #print("seq_lengths")
+    #print(seq_lengths)
     seq_lengths, perm_idx_1 = seq_lengths.sort(0, descending=True)
-    # print(seq_lengths)
-    # print(perm_idx)
-    data = torch.zeros([batch_size, max_seq_len, 40])
+    data = torch.zeros([batch_size, max_seq_len])
     for i in range(0, batch_size):
         x_idx = perm_idx_1[i]
         # print(seqs[x_idx][0].shape)
-        data[i, 0:seq_lengths[i], :] = torch.LongTensor(seqs[x_idx][0])
-
+        data[i, 0:seq_lengths[i]] = torch.LongTensor(seqs[x_idx][0])
+        #print(data[i])
     packed_data = pack_padded_sequence(data, seq_lengths.numpy(), batch_first=True)
 
     lens = torch.zeros(batch_size, dtype=torch.int)
     max_target_len = 0
     for i in range(0, batch_size):
-        lens[i] = seqs[i][1].shape[0] - 1 # omitting leading `<s>`
+        lens[i] = seqs[i][1].shape[0] - 1  # omitting leading `<s>`
         max_target_len = max(max_target_len, lens[i])
 
     target = torch.zeros([batch_size, max_target_len])
@@ -36,10 +36,10 @@ def collate(seqs):
     target_lens = torch.zeros(batch_size, dtype=torch.int)
     for i in range(0, batch_size):
         t_idx = perm_idx_1[i]
-        target_lens[i] = lens[t_idx] - 1 # omitting leading `<s>`
-        target[i, 1:target_lens[i] + 1] = torch.LongTensor(seqs[t_idx][1])
-
-    return [packed_data, target, target_lens, max_target_len]
+        target_lens[i] = lens[t_idx]
+        target[i, 0:target_lens[i]] = torch.LongTensor(seqs[t_idx][1][1:])
+        #print(target[i])
+    return [packed_data, target, target_lens]
 
 
 class Trainset(Dataset):
